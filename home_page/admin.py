5449fde9cc6d32257853180c146a45f9a041c814
beast001 <briyoantony@gmail.com>
2023-06-20 20:25:16 +0300

    dynamic changes

--- home_page/admin.py
+++ home_page/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import My_profile_nav  
+from .models import * 
 
 # Register your models here.
 admin.site.register(My_profile_nav)
+admin.site.register(My_profile_home)
+admin.site.register(testiomonial)
+admin.site.register(My_researche)
+admin.site.register(My_portfolio)
+admin.site.register(My_blog)
+admin.site.register(Contact)
--- home_page/models.py
+++ home_page/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+STATUS = (
+    (0,"not_featured"),
+    (1,"Featured")
+)
+
+
 class My_profile_nav(models.Model):
     name = models.CharField(max_length=100,blank=True, null=True)
     short_intro = models.CharField(max_length=200,blank=True, null=True)
@@ -86,3 +92,112 @@ class My_profile_nav(models.Model):
    
     def __str__(self):
         return self.name
+    
+
+
+class My_profile_home(models.Model):
+    full_name = models.CharField(max_length=100,blank=True, null=True)
+    my_job_status = models.CharField(max_length=100,blank=True, null=True)
+    mid_intro = models.TextField(max_length=600,blank=True, null=True)
+    side_profile_pic = models.ImageField(blank=True, null=True) 
+    what_i_do = models.TextField(max_length=600,blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name    
+    
+
+class testiomonial(models.Model):
+    
+    test_details = models.TextField(max_length=800,blank=True, null=True)
+    test_name = models.CharField(max_length=100,blank=True, null=True)
+    test_job_status = models.CharField(max_length=200,blank=True, null=True)
+    test_profile_pic = models.ImageField(blank=True, null=True)   
+
+    def __str__(self):
+        return self.test_name
+
+
+class My_researche(models.Model):
+
+    research_title = models.CharField(max_length=300,blank=True, null=True)
+    research_short_dis = models.TextField(max_length=600,blank=True, null=True)
+    research_image = models.ImageField(blank=True, null=True)
+    research_published_by = models.CharField(max_length=300,blank=True, null=True)
+    research_publish_time = models.CharField(max_length=100,blank=True, null=True)
+    research_readmore_link = models.URLField(("research_readmore_link"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+    research_publication_link = models.URLField(("research_publication_link"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+
+    Featured = models.IntegerField(choices=STATUS, default=0)
+    
+    def __str__(self):
+        return self.research_title
+
+
+class My_portfolio(models.Model):
+
+    portfolio_title = models.CharField(max_length=300,blank=True, null=True)
+    portfolio_short_dis = models.TextField(max_length=600,blank=True, null=True)
+    portfolio_image = models.ImageField(blank=True, null=True)
+    portfolio_readtime = models.CharField(max_length=100,blank=True, null=True)
+    portfolio_publish_time = models.CharField(max_length=100,blank=True, null=True)
+    portfolio_medium_link = models.URLField(("portfolio_medium_link"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+    portfolio_git_link = models.URLField(("portfolio_git_link"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+
+    portfolio_live_link = models.URLField(("portfolio_live_link"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+
+    Featured = models.IntegerField(choices=STATUS, default=0)
+    
+
+    def __str__(self):
+        return self.portfolio_title
+
+class My_blog(models.Model):
+
+    blog_title = models.CharField(max_length=300,blank=True, null=True)
+    blog_short_dis = models.TextField(max_length=600,blank=True, null=True)
+    blog_image = models.ImageField(blank=True, null=True)
+    blog_readtime = models.CharField(max_length=100,blank=True, null=True)
+    blog_publish_time = models.CharField(max_length=100,blank=True, null=True)
+    blog_medium_link = models.URLField(("blog_medium_link"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+
+    Featured = models.IntegerField(choices=STATUS, default=0)
+    
+
+    def __str__(self):
+        return self.blog_title
+
+class Contact(models.Model):
+    con_name = models.CharField(max_length=100,blank=True, null=True)
+    con_email = models.EmailField(("my_email"), max_length=128, 
+        db_index=True, 
+        unique=False, 
+        blank=True
+    )
+    con_message = models.TextField(max_length=600,blank=True, null=True)
+
+    def __str__(self):
+        return self.con_name
